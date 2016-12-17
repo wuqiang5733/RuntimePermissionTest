b46004194ee88ad356b427a23458ce9e8fd9c85b
xiaoqianghou <2662651103@qq.com>
2016-12-17 17:42:09 +0800

    动态权限

--- app/src/main/java/xuxiaoxiao/com/runtimepermissiontest/MainActivity.java
+++ app/src/main/java/xuxiaoxiao/com/runtimepermissiontest/MainActivity.java
@@ -1,9 +1,12 @@
 package xuxiaoxiao.com.runtimepermissiontest;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -20,12 +23,11 @@ public class MainActivity extends AppCompatActivity {
         makeCall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    Intent intent = new Intent(Intent.ACTION_CALL);
-                    intent.setData(Uri.parse("tel:10086"));
-                    startActivity(intent);
-                } catch (SecurityException e) {
-                    e.printStackTrace();
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+                } else {
+                    call();
                 }
             }
         });
